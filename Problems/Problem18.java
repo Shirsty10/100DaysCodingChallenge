ee0b2d81e3c2baf3c6baf55b4a257ccc14d20f05
Shirsty10 <shirsty1999@gmail.com>
2023-06-01 22:12:55 +0530

    Problem 18

---
+++ Problems/Problem18.java
@@ -0,0 +1,29 @@
+package Problems;
+
+public class Problem18 {
+    
+    static int prefixAndSuffix(String s){
+        if(s.length()<2)
+          return 0;
+          
+          int n= s.length();
+          int count =0;
+          int idx = (n+1)/2;
+
+          while(idx<n){
+            if(s.charAt(idx)!=s.charAt(count)){
+                count = 0;
+                idx = idx-count+1;
+            }else{
+                count++;
+                idx++;
+            }
+          }
+          return count;
+    }
+    public static void main (String[] args)
+    {
+      String s = "abcaabc";
+      System.out.println(prefixAndSuffix(s));
+    }
+}
