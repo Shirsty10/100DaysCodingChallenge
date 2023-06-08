9b0a19fd864a9420bbd2e9ca3cddee2f795e523f
Shirsty10 <shirsty1999@gmail.com>
2023-06-08 22:44:52 +0530

    Problem 25

---
+++ Problems/Problem25.java
@@ -0,0 +1,50 @@
+package Problems;
+
+public class Problem25 {
+    
+    static boolean isOneDistance(String s1,String s2){
+       int l1 = s1.length();
+       int l2 = s2.length();
+
+       if(Math.abs(l1-l2)>1){
+        return false;
+       }
+
+       int i=0,j=0,count=0;
+
+       while(i<l1 && j<l2){
+         
+        if(s1.charAt(i) != s2.charAt(j)){
+            if(count == 1) return false;
+
+            if(l1>l2){
+               i++;
+            }else if(l2>l1){
+               j++;
+            }else{
+                i++;
+                j++;
+
+            }
+            count++;
+        }else{
+            i++;
+            j++;
+        }
+       }
+       if (i < l1 || j < l2)
+       count++;
+
+   return count == 1;
+    }
+    public static void main (String[] args)
+{
+    String s1 = "pep";
+    String s2 = "peepp";
+    if(isOneDistance(s1, s2))
+        System.out.print("Yes");
+    else
+        System.out.print("No");
+}
+
+}
