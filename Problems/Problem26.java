542e56a4ef380a741cc95a79861ff63039aa7f7c
Shirsty10 <shirsty1999@gmail.com>
2023-06-09 22:21:42 +0530

    Problem 26

---
+++ Problems/Problem26.java
@@ -0,0 +1,38 @@
+package Problems;
+
+public class Problem26 {
+     
+    static int saddlePoint(int[][] arr){
+        for(int i=0;i<arr.length;i++){
+            int leastPoint =0;
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j] < arr[i][leastPoint]){
+                    leastPoint = j;
+
+                }
+            }
+            boolean flag = true;
+            for(int k=0;k<arr.length;k++){
+                if(arr[k][leastPoint] > arr[i][leastPoint]){
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag == true){
+               return arr[i][leastPoint];
+                
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        int mat[][] = {{1, 2, 3},
+                      {4, 5, 6},
+                     {7, 8, 9}};
+         
+        
+    System.out.println("Saddle Point : " + saddlePoint(mat));
+    }
+}
