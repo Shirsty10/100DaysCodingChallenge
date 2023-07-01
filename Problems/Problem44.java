3ad83a7519d74a85751334197db1781bf3f2538e
Shirsty10 <shirsty1999@gmail.com>
2023-07-01 16:48:37 +0530

    Problem 44

--- Problems/Problem44.java
+++ Problems/Problem44.java
@@ -20,7 +20,7 @@ public class Problem44 {
       Node slow = head;
       Node fast = head;
 
-      while(fast!=null && fast.next.next!=null){
+      while(fast!=null && fast.next!=null){
         slow = slow.next;
         fast = fast.next.next;
       }
