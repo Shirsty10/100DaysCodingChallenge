75889abbfc50892cdb68bdb956e48e869221d85d
Shirsty10 <shirsty1999@gmail.com>
2023-07-01 15:48:33 +0530

    Problem 44

---
+++ Problems/Problem44.java
@@ -0,0 +1,57 @@
+package Problems;
+
+public class Problem44 {
+
+    static Node head;
+ 
+    static class Node {
+ 
+        int data;
+        Node next;
+ 
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public int middleOfList(){
+      Node slow = head;
+      Node fast = head;
+
+      while(fast!=null && fast.next.next!=null){
+        slow = slow.next;
+        fast = fast.next;
+      }
+
+      return slow.data;
+    }
+
+      /* Inserts a new Node at front of the list. */
+    public void push(int new_data)
+    {
+        /* 1 & 2: Allocate the Node &
+                  Put in the data*/
+        Node new_node = new Node(new_data);
+ 
+        /* 3. Make next of new Node as head */
+        new_node.next = head;
+ 
+        /* 4. Move the head to point to new Node */
+        head = new_node;
+    }
+
+      public static void main(String[] args)
+    {
+        Problem44 llist = new Problem44();
+        llist.push(20);
+        llist.push(4);
+        llist.push(15);
+        llist.push(35);
+         
+          // Function call
+        System.out.println("Middle element of list is : "+ llist.middleOfList());
+    }
+    
+}
